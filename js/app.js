db5b93ee87b6c897a88d9285f9d763eb73acd35f
Keane Quibilan <arv.r2vq@gmail.com>
2022-10-24 19:16:02 -0400

    Add styles and more cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 let loadingDiv = document.getElementById("loading-main");
 let errorDiv = document.getElementById("error-main");
 let deckDiv = document.getElementById("deck");
+let cardHolderDiv = document.getElementById("card-holder");
+
+cardHolderDiv.addEventListener("click", () => {
+  cardHolderDiv.classList.add("hidden");
+});
 
 fetch("/js/deck.json")
   .then(response => {
@@ -11,10 +16,13 @@ fetch("/js/deck.json")
     errorDiv.classList.remove("hidden");
   })
   .then(json => {
-    console.log(json);
     return json.deck.map(card => {
       let cardDiv = document.createElement("div");
       cardDiv.classList.add("list-item");
+
+      cardDiv.addEventListener("click", () => {
+        cardHolderDiv.classList.remove("hidden");
+      });
 
       let nameDiv = document.createElement("div");
       nameDiv.classList.add("list-item-name");
